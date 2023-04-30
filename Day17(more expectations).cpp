3bef513f4ae586f55841004221ed0150c8805435
Anjali Vishwakarma <92050489+Anjali-987-vish@users.noreply.github.com>
2023-05-01 00:02:17 +0530

    Update Day17(more expectations).cpp

--- Day17(more expectations).cpp
+++ Day17(more expectations).cpp
@@ -1,1 +1,9 @@
-hevwkfvewflw
+class Calculator {
+public:
+    int power(int n, int p) {
+        if (n < 0 || p < 0) {
+            throw invalid_argument("n and p should be non-negative");
+        }
+        return pow(n, p);
+    }
+};
